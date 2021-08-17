255d16f07041d580483e1880e9fea8e533e4bb76
sourabh <sourabhrahafbg@gmail.com>
2021-08-18 02:56:26 +0530

    initial

---
+++ src/components/loader/loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Loader() {
+    return (
+        <div class="loader-wrapper">
+          <div class="loader"></div>
+        </div>
+      
+        )
+}
+
+export default Loader
